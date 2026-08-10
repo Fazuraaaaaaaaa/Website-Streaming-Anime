9c26f76c019d18eb9eb8e04056d55106e77fddff
Fazuraaaaaaaaa <rafarizky736@gmail.com>
2026-08-10 07:10:52 +0700

    fix: downgrade cheerio to 1.0.0-rc.12 to fix nextjs bundle crash

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['cheerio', 'yt-search'],
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
